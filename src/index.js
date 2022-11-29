fb35f4eed21d315b1e2cc54b0948468686ecda76
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-29 09:41:04 -0800

    Write the button component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Button = (props) => {
-  // your code here
+  return (
+    <button>
+      {props.children}
+    </button>
+  );
 };
 
 const Application = () => {
@@ -17,6 +21,7 @@ const Application = () => {
 
   return (
     <main>
+      <Button>Reset</Button>
       {/* your code here -- this entire line including the curly braces can be removed */}
       <h1>Hello React</h1>
     </main>
